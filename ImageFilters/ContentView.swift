58a028d15e31e16f59bc32c11172616dabb37d24
Dzmitry Navak <d_navak@flo.health>
2023-03-30 22:18:13 +0300

    Initial Commit

---
+++ ImageFilters/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImageFilters
+//
+//  Created by oroom on 2023-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageFilters/ImageFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFiltersApp.swift
+//  ImageFilters
+//
+//  Created by oroom on 2023-03-30.
+//
+
+import SwiftUI
+
+@main
+struct ImageFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
